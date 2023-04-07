459fca546fdf5e0aff10f7a0b95539db6b5677e9
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-04-07 11:15:07 +0300

    Fixed not found layout

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import Layout from "../../components/Layout/Layout";
 import MainButton from "../../components/MainButton/MainButton";
 import NotFoundStyles from "./NotFound.module.scss";
 
@@ -7,10 +6,10 @@ const NotFound = () => {
     const navigate = useNavigate();
     return (
         <main className={NotFoundStyles.main}>
-            <Layout className={NotFoundStyles.not_found}>
+            <section className={NotFoundStyles.not_found}>
                 <h1>Oops, Looks like the page you were looking for does not exist!</h1>
                 <MainButton label="Return Home" onClick={() => navigate("/")} />
-            </Layout>
+            </section>
         </main>
     );
 };
